ed7040fd66dfa62cf828c645ddeadcddda6b1c77
Kyeongukna <ruddnr96@naver.com>
2019-11-23 21:06:11 +0900

    controller update

--- src/main/java/controller/LadderManager.java
+++ src/main/java/controller/LadderManager.java
@@ -1,16 +1,15 @@
 package controller;
 
 import model.Ladder;
+import model.LadderResults;
 import model.Line;
 import model.Users;
-import view.InputLadderHeight;
-import view.InputUser;
-import view.PrintLadder;
-import view.PrintUser;
+import view.*;
 
 public class LadderManager {
     private Ladder ladder;
     private Users users;
+    private PrintResult printResult;
 
     public LadderManager() {
         initLadder();
@@ -20,15 +19,20 @@ public class LadderManager {
     private void initLadder() {
         InputUser inputUser = new InputUser();
         users = new Users (inputUser.getUserNames());
+        InputResult inputResult = new InputResult(users.getNameLength());
+        printResult = new PrintResult(inputResult.getResults());
         InputLadderHeight inputLadderHeight = new InputLadderHeight();
-        ladder = new Ladder(users.getNames().size(), inputLadderHeight.getHeight());
+        ladder = new Ladder(users.getNameLength(), inputLadderHeight.getHeight());
     }
 
     private void startLadder() {
         new PrintUser(users.getNames());
-        for(Line line: ladder.getLadder()) {
+        for(Line line: ladder.getLadders()) {
             new PrintLadder(line.getPoint());
         }
+        printResult.printLadderResults();
+        LadderResults ladderResults = new LadderResults(ladder);
+        printResult.selectUser(users.getNames(), ladderResults.getLadderResult());
     }
 
 }
